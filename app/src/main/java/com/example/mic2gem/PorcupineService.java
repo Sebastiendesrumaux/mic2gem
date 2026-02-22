dd24e2f0ec6658f5445b93f012ba76eb51a81a1b
Sebastiendesrumaux@gmail.com <sebastien.desrumaux@gmail.com>
2026-02-22 13:09:55 +0100

    Save 2026-02-22 13:09:55

--- app/src/main/java/com/example/mic2gem/PorcupineService.java
+++ app/src/main/java/com/example/mic2gem/PorcupineService.java
@@ -89,7 +89,7 @@ public class PorcupineService extends Service {
         handler.postDelayed(() -> {
             Intent macroIntent = new Intent("mic2gem_intent");
             sendBroadcast(macroIntent);
-            resumeListeningAfterDelay(7000);
+            resumeListeningAfterDelay(5000);
         }, 2000);
     }
 
